f0b0a026ed016477e1580501ed1489225a155c2f
Joseph DeVictoria <jldevictoria@gmail.com>
2014-10-17 12:00:51 -0600

    I kinda messed up master

--- Space_Invaders_Lab_4_WS/space_invaders/Debug/src/bitmap.d
+++
@@ -1,6 +0,0 @@
-src/bitmap.d src/bitmap.o: ../src/bitmap.c ../src/bitmap.h \
-  ../src/globals.h
-
-../src/bitmap.h:
-
-../src/globals.h:
--- Space_Invaders_Lab_4_WS/space_invaders/Debug/src/collisions.d
+++
@@ -1,3 +0,0 @@
-src/collisions.d src/collisions.o: ../src/collisions.c ../src/globals.h
-
-../src/globals.h:
